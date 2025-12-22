e958c9c49fdd50bc1151cf2953063d71112bee9d
Dozergo <hlafaye2@gmail.com>
2025-12-22 22:10:57 +0100

    Initial commit: Smart City Insights

---
+++ main.py
@@ -0,0 +1,5 @@
+from flask import Flask, render_template, redirect, url_for, flash, request, session, send_file
+from  flask_bootstrap import Bootstrap5
+import base64
+import os 
+from dotenv import load_dotenv
